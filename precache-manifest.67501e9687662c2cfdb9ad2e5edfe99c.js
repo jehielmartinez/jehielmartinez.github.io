354718d2db71db58c8b7af55c9c88f7d89f5b29e
Jehiel Martinez <38274012+jehielmartinez@users.noreply.github.com>
2020-02-09 17:44:15 -0600

    cryptos

---
+++ precache-manifest.67501e9687662c2cfdb9ad2e5edfe99c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef06c7cc3f12bf6db7b41350930562d3",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "be99f2f2a3a3286feb0a",
+    "url": "/portfolio-website/static/css/main.c5ede8ab.chunk.css"
+  },
+  {
+    "revision": "071be57cc46c88a19646",
+    "url": "/portfolio-website/static/js/2.9463e4f4.chunk.js"
+  },
+  {
+    "revision": "5c40b6d5fb7ba1485ed43e324a0bc563",
+    "url": "/portfolio-website/static/js/2.9463e4f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be99f2f2a3a3286feb0a",
+    "url": "/portfolio-website/static/js/main.3d6c306d.chunk.js"
+  },
+  {
+    "revision": "6a928cff6e7ae358a8bf",
+    "url": "/portfolio-website/static/js/runtime-main.65e56ccf.js"
+  },
+  {
+    "revision": "aec03b597ba00256965feabaff9ee700",
+    "url": "/portfolio-website/static/media/Jehiel_Martinez_CV.aec03b59.pdf"
+  },
+  {
+    "revision": "57c8733756914ba817308668265bc734",
+    "url": "/portfolio-website/static/media/beco.57c87337.png"
+  },
+  {
+    "revision": "9062013b2233e6f7f63a61e21bc1fc90",
+    "url": "/portfolio-website/static/media/cryptos.9062013b.png"
+  },
+  {
+    "revision": "10a2f0d4c6f8ef4a4bb674f50b52dd74",
+    "url": "/portfolio-website/static/media/oversight.10a2f0d4.jpg"
+  },
+  {
+    "revision": "2584c25f74314095ac117bc322943ffa",
+    "url": "/portfolio-website/static/media/playground.2584c25f.jpg"
+  },
+  {
+    "revision": "bd8e5c2792a7088dea66afd27dabdf5d",
+    "url": "/portfolio-website/static/media/profile-pic.bd8e5c27.jpg"
+  },
+  {
+    "revision": "5bffc909766f25ef9f25134c0ce83068",
+    "url": "/portfolio-website/static/media/r-tracker.5bffc909.jpg"
+  },
+  {
+    "revision": "4744ee47052b0857e816f8d4750eeeb5",
+    "url": "/portfolio-website/static/media/stock-control.4744ee47.jpg"
+  }
+]);

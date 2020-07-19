700a1fdfce6c506fd51e91dc8432ac05d2074ceb
jehielmartinez <jehielmartinez@users.noreply.github.com>
2020-07-19 19:30:36 +0000

    deploy: 6da98f5ad7bb1b66764aee952dd2dde9c54b8023

--- README.md
+++
@@ -1,6 +0,0 @@
-## Personal Portfolio
-
-This repo contains the code for my personal portfolio website created with React 16.9.0
-Feel free to use the components for your own  websites changing the resume.json file with your own data.
-
-visit [jehielmartinez.com](https://jehielmartinez.com)
---
+++ precache-manifest.7056c45d00dfe6704e1a0065471df3b9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee67d9c5ce48634f6049dbfa1c22b76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d7ebb4b8cab772b654",
+    "url": "/static/css/main.91343789.chunk.css"
+  },
+  {
+    "revision": "504d2f315d47095bc9bd",
+    "url": "/static/js/2.730a1f4b.chunk.js"
+  },
+  {
+    "revision": "7c3ee0b9ab2f5f13c4e3ace24eac1cbe",
+    "url": "/static/js/2.730a1f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d7ebb4b8cab772b654",
+    "url": "/static/js/main.a90ecd04.chunk.js"
+  },
+  {
+    "revision": "d98441b121ed41603432",
+    "url": "/static/js/runtime-main.9f7fa874.js"
+  },
+  {
+    "revision": "aec03b597ba00256965feabaff9ee700",
+    "url": "/static/media/Jehiel_Martinez_CV.aec03b59.pdf"
+  },
+  {
+    "revision": "57c8733756914ba817308668265bc734",
+    "url": "/static/media/beco.57c87337.png"
+  },
+  {
+    "revision": "d29981ab3347ad517eeb5aaf6000a5bb",
+    "url": "/static/media/codeExitos.d29981ab.png"
+  },
+  {
+    "revision": "9062013b2233e6f7f63a61e21bc1fc90",
+    "url": "/static/media/cryptos.9062013b.png"
+  },
+  {
+    "revision": "10a2f0d4c6f8ef4a4bb674f50b52dd74",
+    "url": "/static/media/oversight.10a2f0d4.jpg"
+  },
+  {
+    "revision": "2584c25f74314095ac117bc322943ffa",
+    "url": "/static/media/playground.2584c25f.jpg"
+  },
+  {
+    "revision": "bd8e5c2792a7088dea66afd27dabdf5d",
+    "url": "/static/media/profile-pic.bd8e5c27.jpg"
+  },
+  {
+    "revision": "5bffc909766f25ef9f25134c0ce83068",
+    "url": "/static/media/r-tracker.5bffc909.jpg"
+  },
+  {
+    "revision": "4744ee47052b0857e816f8d4750eeeb5",
+    "url": "/static/media/stock-control.4744ee47.jpg"
+  }
+]);
